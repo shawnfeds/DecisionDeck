7138b0df98195a2c99c87d98e4dac9441bd595ec
shawnfeds <shawnfernandes256@gmail.com>
2025-02-26 14:39:19 +0530

    added the missed files

---
+++ wwwroot/js/pollLink.js
@@ -0,0 +1,6 @@
+﻿import { getCookie } from "./cookie.js";
+
+document.getElementById('poll').onclick = function () {
+    var userId = getCookie("UserId");
+    window.location.href = "/Poll/Index?UserId=" + userId;
+}
